245fa4712d6fb82003094850c9bed9b74ea6bfb3
SHOXRUXno1 <brawlstars7471255@gmail.com>
2023-10-19 21:45:54 +0500

    Create Homework.py

---
+++ Homework.py
@@ -0,0 +1,22 @@
+def Foydalanuvchi():
+    """Kvadrat va kubni konsolga chiqaruvchi funksiya"""
+    print(Foydalanuvchi.__doc__)
+    square = int(input("Hohlagan natural son yozing>>"))
+    print(f"{square} ning kavadrati, = {square ** 2} ga teng")
+    print(f"{square} ning kubi, = {square ** 3} ga teng")
+
+
+Foydalanuvchi()
+
+
+def Foydalanuvchi1():
+    """Juft yoki Toqligini tekshiruvchi funksiya"""
+    print(Foydalanuvchi1.__doc__)
+    number = int(input("Hohlagan natural son yozing>>"))
+    if (number % 2) == 0:
+        print(f"{number} Juft son")
+    else:
+        print(f"{number} Toq")
+
+
+Foydalanuvchi1()
